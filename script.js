5c6d12aa17d2db891d5de897be48f9a061478784
leoosouza <leonardo.contatosouza@gmail.com>
2022-09-27 19:25:21 -0300

    conclusao-027

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let primeiraFruta=document.getElementById('fruta-1')
+
+primeiraFruta.innerHTML=frutas[0]
+
+let segundaFruta=document.getElementById('fruta-2')
+
+segundaFruta.innerHTML=frutas[1]
+
+let terceiraFruta=document.getElementById('fruta-3')
+
+terceiraFruta.innerHTML=frutas[2]
+
+// let quartaFruta=document.getElementById('fruta-4')
+// quartaFruta.innerHTML=''
+
+let input=document.getElementById('nova-fruta')
+
+function frutosa() {
+    let colocando=document.getElementById('fruta-4').innerHTML=input.value
+}
